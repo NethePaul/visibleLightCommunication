a28e93e8573b6ef81f8639101f867f82586940a2
Paul Nethe <paul04nethe@gmail.com>
2026-08-10 19:12:00 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# visibleLightCommunication
+# Polarization communication
+This is a proof of concept for a steganographic communication channel using polarization of light
+
+
+## Encoder
+### Hardware
+The code is compatible with any arduino compatible microcontroller like the arduino uno or the eps32.
+### Flash
+The provided Makefile uses arduino-cli to compile and upload the code.
+You need to install arduino-cli and run `arduino-cli core install esp32:esp32` or `arduino-cli core install arduino:avr`
+Then, run `make upload` to compile and upload the code to an esp32 or `make upload_uno` to upload the code to an arduino uno.
+You can also flash the code using the Ardunio IDE.
+### Webserver
+If the microcontroller has a WiFi module, uncomment line 3, and set the SSID and password of your router on line 84 and 85. This will enable a Webserver on the encoder, which allows dynamically changing the message via a `HTTP POST [$ip]:80/set?data=$new_message` request.
+The IP address of the microcontroller can be found using the administration page of the connected router.
+Otherwise the message can only be changed by reflashing the microcontroller with a different hardcoded message.
+
+### Sending
+The mikrocontroller will continuously send the message in a loop while it is powered.
+
+## Decoder
+### Hardware
+The code runs on a linux device with a camera descriptor on `/dev/video0`.
+### Configuration
+The camera is configured via `make camera_config` which uses `cameractrls` as a dependency.
+### Compilation
+Run `make`, which requires opencv4 as a dependency and a c++ compiler.
+### Decoding
+The output contains debug information which can be filtered out via `grep 'received string:'`.
+#### Live decoding
+Run `bin/decoder /dev/video0` after compilation or `make live`.
+#### Decoding from a video file
+Run `bin/decoder video.mp4` after compilation.
+#### Adjusting the minimum Threshold
+Run `bin/decoder $video_source $minimum_threshold`.
--- encoder/encoder.cpp
+++ encoder/encoder.cpp
@@ -1,117 +1,145 @@
-#include<Arduino.h>
-#include<WiFi.h>
-#include<WebServer.h>
-#include<Preferences.h>
+#include <Arduino.h>
+
+#define WEBSERVER 1
+
+#ifdef WEBSERVER
+#include <Preferences.h>
+#include <WebServer.h>
+#include <WiFi.h>
+#endif
 
 #define LED 5
-// #define START 22
-// #define END 23
 
-class Data{
-  unsigned bi=0;
-  int8_t ci=7+8;
-  String data="";
-  unsigned len=0;
+class Data {
+    unsigned bi = 0;
+    int8_t ci = 7 + 8;
+    String data = "";
+    unsigned len = 0;
+
 public:
-  bool end(){return bi>len+1;};
-  void set(String data){
-    len=data.length();
-    this->data=data;
-    reset();
-  }
-  void reset(){
-    bi=0;
-    ci=7+12;
-  }
-  bool getNextBit(){
-    if(ci>=8){
-      ci--;
-      return 1;
+    bool end() { return bi > len + 1; };
+    void set(String data)
+    {
+        len = data.length();
+        this->data = data;
+        reset();
     }
-    bool d=0;
-      
-    
-    if(end())return 0;
-    
-    if(bi<len){
-      d = (data[bi]>>ci)&1;
+    void reset()
+    {
+        bi = 0;
+        ci = 7 + 12;
     }
-    ci--;
+    bool getNextBit()
+    {
+        if (ci >= 8) {
+            ci--;
+            return 1;
+        }
+        bool d = 0;
 
-    if(ci<0){
-      ci=7;
-      bi++;
+        if (end())
+            return 0;
+
+        if (bi < len) {
+            d = (data[bi] >> ci) & 1;
+        }
+        ci--;
+
+        if (ci < 0) {
+            ci = 7;
+            bi++;
+        }
+
+        return d;
     }
-    
-    return d;
-  }
 };
 
 Data data;
 
 unsigned long start;
-WebServer server(80);
 
+#ifdef WEBSERVER
+
+WebServer server(80);
 Preferences p;
 
-void set_data(){
-  if(server.args()==0)return;
-  auto a1=server.arg(0);
-  data.set(a1.c_str());
-  p.putString("data",a1);
-  server.send(200);
+void set_data()
+{
+    if (server.args() == 0)
+        return;
+    auto a1 = server.arg(0);
+    data.set(a1.c_str());
+    p.putString("data", a1);
+    server.send(200);
 };
 
-void not_found(){
-  server.send(404);
+void not_found()
+{
+    server.send(404);
 }
 
-void setup(){
-  p.begin("my-prefrence");
-  WiFi.begin("ssid","password");
-  server.on("/set", set_data);
-  server.onNotFound(not_found);
-  server.begin(80);
-  pinMode(LED, OUTPUT);
-  // pinMode(END, OUTPUT);
-  // pinMode(START, INPUT);
-  
-  digitalWrite(LED, LOW);
-  String str = p.getString("data","Hello World");
-  data.set(str);
-  start=millis();
+void setup()
+{
+    p.begin("my-prefrence");
+    WiFi.begin("ssid", "password");
+    server.on("/set", set_data);
+    server.onNotFound(not_found);
+    server.begin(80);
+    pinMode(LED, OUTPUT);
+    // pinMode(END, OUTPUT);
+    // pinMode(START, INPUT);
+
+    digitalWrite(LED, LOW);
+    String str = p.getString("data", "Hello World");
+    data.set(str);
+    start = millis();
 }
 
-void set(bool a){
-  int led=LED;
-  digitalWrite(led, a?LOW:HIGH);
- }
+#else
 
+void setup()
+{
+    pinMode(LED, OUTPUT);
+    // pinMode(END, OUTPUT);
+    // pinMode(START, INPUT);
 
-void loop(){
-  server.handleClient();
-  static bool a=true;
-  // if(digitalRead(START)==LOW)return;
-  set(a);
-  a=!a;
+    digitalWrite(LED, LOW);
+    data.set("Hello World");
+    start = millis();
+}
 
-  bool bit=data.getNextBit();
-  
-  if(bit){
-    start+=130;
-    unsigned long d=millis();
-    delay(start-d);
-  }
-  else{
-    start+=60;
-    unsigned long d=millis();
-    delay(start-d);
-  }
+#endif
 
-  if(data.end()){
-    data.reset();
-    // digitalWrite(END, HIGH);
-    // delay(1);
-    // digitalWrite(END, LOW);
-  }
+void set(bool a)
+{
+    int led = LED;
+    digitalWrite(led, a ? LOW : HIGH);
+}
+
+void loop()
+{
+
+#ifdef WEBSERVER
+    server.handleClient();
+#endif
+
+    static bool a = true;
+    set(a);
+    a = !a;
+
+    bool bit = data.getNextBit();
+
+    if (bit) {
+        start += 130;
+        unsigned long d = millis();
+        delay(start - d);
+    } else {
+        start += 60;
+        unsigned long d = millis();
+        delay(start - d);
+    }
+
+    if (data.end()) {
+        data.reset();
+    }
 }
